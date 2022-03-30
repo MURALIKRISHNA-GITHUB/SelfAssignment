6fc17f5ff2edaaf991a44d41eadd423e202ac3de
MURALIKRISHNA-GITHUB <52492248+MURALIKRISHNA-GITHUB@users.noreply.github.com>
2022-03-30 11:04:39 +0530

    Add files via upload

---
+++ Models/SelfAssignmentContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SelfAssignment_MvcVersion.Models
+{
+    public partial class SelfAssignmentContext : DbContext
+    {
+        public SelfAssignmentContext()
+        {
+        }
+
+        public SelfAssignmentContext(DbContextOptions<SelfAssignmentContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<UserInfo> UserInfos { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=ATPLAPTOP24\\SQLEXPRESS;Initial Catalog=SelfAssignment;Persist Security Info=True;User ID=localhost;Password=Window@123");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserInfo>(entity =>
+            {
+                entity.ToTable("UserInfo");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SelfAssignment_MvcVersion.Models
+{
+    public partial class UserInfo
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public int LastName { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
